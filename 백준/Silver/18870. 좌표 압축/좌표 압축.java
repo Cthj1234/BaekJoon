e5e6a2bf2ed5557f87547177aa6e30ddb76adcb2
cthj1234 <114575847+Cthj1234@users.noreply.github.com>
2023-12-04 01:31:35 +0900

    [Silver II] Title: 좌표 압축, Time: 2360 ms, Memory: 278284 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] tmp = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            tmp[i] = arr[i];
+        }
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        Arrays.sort(tmp);
+        int Min = tmp[0];
+        int count = 0;
+        for (int i = 0; i < tmp.length; i++) {
+            if(Min < tmp[i]){
+                hashMap.put(tmp[i], ++count);
+            }
+            Min = tmp[i];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+           sb.append(hashMap.getOrDefault(arr[i],0) + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
