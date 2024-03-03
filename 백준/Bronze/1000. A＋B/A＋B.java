8d6c30f16ff6dbe3b0a8848bff863a42b903a85b
cthj1234 <114575847+Cthj1234@users.noreply.github.com>
2024-03-03 16:32:00 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int n2 = sc.nextInt();
+		int answer = n + n2;
+		System.out.println(answer);
+	}
+}
